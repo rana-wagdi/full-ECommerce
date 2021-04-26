da04ddbbe871c6db6ef303c2719a34afb27300cc
rana2020701 <ranawagdi4@gmail.com>
2021-04-26 08:05:57 +0200

    add rating

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,30 +1,28 @@
 import React from 'react';
+import Rating from './Rating';
+
 import Data from './Data';
 function Main() {
     return(
         <main className='main'>
             <div className='row center'>
            {Data.products.map((product)=>(
-                   <div key={product._id} className='card'>
-                       <a href={`/product/${product._id}`}>
-                           <img className='medium' src={product.image} alt={product.name} />
-                       </a>
-                <div className='card-body'>
+            <div key={product._id} className='card'>
+             <a href={`/product/${product._id}`}>
+            <img className='medium' src={product.image} alt={product.name} />
+         </a>
+            <div className='card-body'>
                 <a href={`/product/${product._id}`}>
                 <h2>Nike Slim Shirts</h2>
                 </a>
-                <div className='rating'>
-                <span><i className='fa fa-star'></i> </span>
-                <span><i className='fa fa-star'></i></span>
-                <span><i className='fa fa-star'></i></span>
-                <span><i className='fa fa-star'></i></span>
-                <span><i className='fa fa-star-half-o'></i></span>
+                <Rating rating={product.rating} numReviews={product.numReviews} />
+                   
+                            <div className='price'>
+                        ${product.price}
+                        </div>
                 </div>
-                <div className='price'>
-                ${product.price}
-                </div>
-                </div>
-                   </div>
+    </div>
+
                ))
            }
             </div>
