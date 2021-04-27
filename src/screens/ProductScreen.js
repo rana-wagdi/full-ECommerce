0bdec924e245b32c7b3d69fafe20a91709405f30
rana2020701 <ranawagdi4@gmail.com>
2021-04-27 09:45:02 +0200

    Update ProductScreen.js

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Data from '../components/Data';
+import Rating from '../components/Rating'
 
-const ProductScreen = () =>  {
+const ProductScreen = (props) =>  {
+    const product = Data.products.find((x) => x._id === props.match.params.id);
+
+    if(!product) {
+        return <div>Product Not Found</div>
+    }
     return (
         <div>
-            product Screen
+             <Link to="/">Back to result</Link>
+        <div className='row'>
+        <div className='col-2'>
+        <img className="large" src={props.image} alt={product.name}></img>
+            </div>
+        
+        <div className='col-1'>
+            <ul>
+                <li>
+                    <h1>{product.name}</h1>
+                </li>
+                <li>
+                    <Rating rating={product.rating} numReviews={product.numReviews} />
+                </li>
+                <li>Price : ${product.price} </li>
+                <img className='medium' src={product.image} alt={product.name} />
+            </ul>
+
         </div>
+        <div className='col-1'>
+
+        </div>
+            </div>
+            </div>
+        
+        
     )
 }
 
