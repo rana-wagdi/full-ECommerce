f8c3baa0eea0c1463658ff7040f9d781275d9fa1
rana2020701 <ranawagdi4@gmail.com>
2021-04-27 10:05:58 +0200

    add image add card and details

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -14,7 +14,7 @@ const ProductScreen = (props) =>  {
              <Link to="/">Back to result</Link>
         <div className='row'>
         <div className='col-2'>
-        <img className="large" src={props.image} alt={product.name}></img>
+        <img className="large" src={product.image} alt={product.name}></img>
             </div>
         
         <div className='col-1'>
@@ -26,12 +26,36 @@ const ProductScreen = (props) =>  {
                     <Rating rating={product.rating} numReviews={product.numReviews} />
                 </li>
                 <li>Price : ${product.price} </li>
-                <img className='medium' src={product.image} alt={product.name} />
+                <li>
+                    Description: <p>{product.description}</p>
+                </li>
             </ul>
 
         </div>
         <div className='col-1'>
-
+        <div className='card card-body'>
+        <ul>
+            <li>
+                <div className='row'>
+                <div>Price</div>
+                <div className='price'>${product.price}</div>
+                </div>
+            </li>
+            <li>
+                <div className='row'>
+                <div>Status</div>
+                <div >  {product.countInStock > 0 ? (
+                      <span className="success">In Stock</span>
+                    ) : (
+                      <span className="error">Unavailable</span>
+                    )}</div>
+                </div>
+            </li>
+            <li>
+                <button className="primary block">Add to Cart</button>
+              </li>
+        </ul>
+        </div>
         </div>
             </div>
             </div>
