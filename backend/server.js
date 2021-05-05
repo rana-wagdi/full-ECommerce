960db24632e39fecbc4da9f64f75ed3e97e59af6
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-05-05 04:14:02 -0700

    add nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from 'express';
-
+import Data from './Data.js';
 
 const app = express();
+
+
+app.get('/api/products', (req, res)=>{
+    res.send(Data.products)
+})
 
 app.get('/', (req, res)=> {
     res.send('Server is ready');
